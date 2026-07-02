efdbaebf93002a958782a757ab379c1fcfd5529b
Alan Avila <22151196@aguascalientes.tecnm.mx>
2026-07-02 09:49:28 -0600

    Detalle_Producto
    Detalle producto, booton agergar productos, relacionados

---
+++ productos.js
@@ -0,0 +1,66 @@
+const inventario = [
+  { 
+    id: 1, 
+    nombre: "Don Julio Reposado", 
+    precio: 1500.00, 
+    imagen: "../Imagenes/donjulioreposado.jpeg", 
+    url: "DonJulioRep.html", 
+    etiquetas: ["reposado", "don-julio"] 
+  },
+  { 
+    id: 2, 
+    nombre: "Jose Cuervo", 
+    precio: 1300.00, 
+    imagen: "../Imagenes/josecuervo.jpg", 
+    url: "JoseCuervo.html", 
+    etiquetas: ["reposado", "jose-cuervo"] 
+  },
+  { 
+    id: 3, 
+    nombre: "Campo Azul", 
+    precio: 1200.00, 
+    imagen: "../Imagenes/campoazul.jpg", 
+    url: "CampoAzul.html", 
+    etiquetas: ["blanco", "campo-azul"] 
+  },
+  { 
+    id: 4, 
+    nombre: "1800 Cristal", 
+    precio: 1550.00, 
+    imagen: "../Imagenes/1800cristal.webp", 
+    url: "1800cristal.html", 
+    etiquetas: ["cristalino", "añejo", "1800"] 
+  },
+  { 
+    id: 5, 
+    nombre: "1800", 
+    precio: 1800.00, 
+    imagen: "../Imagenes/1800.jpg", 
+    url: "1800.html", 
+    etiquetas: ["reposado", "1800"] 
+  },
+  { 
+    id: 6, 
+    nombre: "Circulo", 
+    precio: 2100.00, 
+    imagen: "../Imagenes/Circulo.webp", 
+    url: "Circulo.html", 
+    etiquetas: ["blanco", "circulo"] 
+  },
+  { 
+    id: 7, 
+    nombre: "Don Julio Ultima Reserva", 
+    precio: 4500.00, 
+    imagen: "../Imagenes/DonJulioUltima.webp", 
+    url: "DonJulioUltima.html", 
+    etiquetas: ["extra-añejo", "don-julio", "premium"] 
+  },
+  { 
+    id: 8, 
+    nombre: "Ley .925", 
+    precio: 2500.00, 
+    imagen: "../Imagenes/Ley925.jpg", 
+    url: "Ley925.html", 
+    etiquetas: ["blanco", "premium", "ley-925"] 
+  }
+];
