891bf7363a850c94b8e798f4e378c848e08bdb82
Alan Avila <22151196@aguascalientes.tecnm.mx>
2026-07-13 11:25:11 -0600

    Escaparate_Promociones_Cookies
    Promociones, cookies y escaparate

---
+++ ofertas.js
@@ -0,0 +1,29 @@
+const promocionesActivas = [
+  { 
+    id: 1, 
+    nombre: "20% de desuento en reposados", 
+    tipo: "descuento", 
+    valor: 20, 
+    productoNombre: "Todos los Reposados", 
+    descripcion: "Aprovecha el descuento en toda la línea de reposados.",
+    codigo: "REPOSADO20"
+  },
+  { 
+    id: 2, 
+    nombre: "2x1", 
+    tipo: "2x1", 
+    valor: null, 
+    productoNombre: "", 
+    descripcion: "Llévate 2 botellas al precio de 1.",
+    codigo: "DOSPORUNO"
+  },
+  { 
+    id: 3, 
+    nombre: "3x1 en Don Julio", 
+    tipo: "3x1", 
+    valor: null, 
+    productoNombre: "Todos los productos de Don Julio", 
+    descripcion: "Llevate 3 botellas de don julio al precio de 1",
+    codigo: "TRESJULIOS"
+  }
+];
