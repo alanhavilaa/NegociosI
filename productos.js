68ab93f8caaa385f8b3ac8fe5ba54d0cb7c282b5
Alan Avila <22151196@aguascalientes.tecnm.mx>
2026-07-20 09:39:28 -0600

    UltimoCambio
    ultimoCambio

--- productos.js
+++ productos.js
@@ -3,7 +3,7 @@ const inventario = [
     id: 1, 
     nombre: "Don Julio Reposado", 
     precio: 1500.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/donjulioreposado.jpeg", 
+    imagen: "Imagenes/donjulioreposado.jpeg", 
     url: "DonJulioRep.html", 
     etiquetas: ["reposado", "don-julio"] 
   },
@@ -11,7 +11,7 @@ const inventario = [
     id: 2, 
     nombre: "Jose Cuervo", 
     precio: 1300.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/josecuervo.jpg", 
+    imagen: "Imagenes/josecuervo.jpg", 
     url: "JoseCuervo.html", 
     etiquetas: ["reposado", "jose-cuervo"] 
   },
@@ -19,7 +19,7 @@ const inventario = [
     id: 3, 
     nombre: "Campo Azul", 
     precio: 1200.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/campoazul.jpg", 
+    imagen: "Imagenes/campoazul.jpg", 
     url: "CampoAzul.html", 
     etiquetas: ["añejo", "campo-azul"] 
   },
@@ -27,7 +27,7 @@ const inventario = [
     id: 4, 
     nombre: "1800 Cristal", 
     precio: 1550.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/1800cristal.webp", 
+    imagen: "Imagenes/1800cristal.webp", 
     url: "1800cristal.html", 
     etiquetas: ["cristalino", "blanco" , "1800"] 
   },
@@ -35,7 +35,7 @@ const inventario = [
     id: 5, 
     nombre: "1800", 
     precio: 1800.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/1800.jpg", 
+    imagen: "Imagenes/1800.jpg", 
     url: "1800.html", 
     etiquetas: ["blanco", "1800"] 
   },
@@ -43,7 +43,7 @@ const inventario = [
     id: 6, 
     nombre: "Circulo", 
     precio: 2100.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/Circulo.webp", 
+    imagen: "Imagenes/Circulo.webp", 
     url: "Circulo.html", 
     etiquetas: ["blanco", "premium", "circulo"] 
   },
@@ -51,7 +51,7 @@ const inventario = [
     id: 7, 
     nombre: "Don Julio Ultima Reserva", 
     precio: 4500.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/DonJulioUltima.webp", 
+    imagen: "Imagenes/DonJulioUltima.webp", 
     url: "UltimaReserva.html", 
     etiquetas: ["extra-añejo", "añejo","don-julio", "premium"] 
   },
@@ -59,7 +59,7 @@ const inventario = [
     id: 8, 
     nombre: "Ley .925", 
     precio: 2500.00, 
-    imagen: "file:///C:/Users/eduar/Desktop/NegociosI/Imagenes/Ley925.jpg", 
+    imagen: "Imagenes/Ley925.jpg", 
     url: "Ley095.html", 
     etiquetas: ["blanco", "premium", "ley-925"] 
   }
